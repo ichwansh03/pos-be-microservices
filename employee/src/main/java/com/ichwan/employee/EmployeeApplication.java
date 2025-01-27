1444c1954cc26e30b4bd6ed2e2dfe09e48cd3e0e
ichwansh03 <ichwansholihin70@gmail.com>
2025-01-27 11:35:04 +0700

    basic: add audit annotations

--- employee/src/main/java/com/ichwan/employee/EmployeeApplication.java
+++ employee/src/main/java/com/ichwan/employee/EmployeeApplication.java
@@ -2,8 +2,10 @@ package com.ichwan.employee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
 public class EmployeeApplication {
 
 	public static void main(String[] args) {
