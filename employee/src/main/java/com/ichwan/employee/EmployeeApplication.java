d3397796d7ed2dea396b83e9b0c82bc02df15678
ichwansh03 <ichwansholihin70@gmail.com>
2025-04-09 06:22:54 +0700

    register eureka client

--- employee/src/main/java/com/ichwan/employee/EmployeeApplication.java
+++ employee/src/main/java/com/ichwan/employee/EmployeeApplication.java
@@ -2,6 +2,7 @@ package com.ichwan.employee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
